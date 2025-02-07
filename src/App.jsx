bb5d2e76dd1292c9edd975432637f99b3f51b5ab
Danil Faizullin <toxen2005@mail.ru>
2025-02-07 16:34:51 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+// import Education from "./components/Education/Education.jsx"
+// import Form from './components/Form/Form.jsx'
+// import ReducerTest from './components/Education/ReducerTest.jsx'
+// import TestForm from './components/TestForm/TestForm.jsx'
+// import MainPage from './components/SiteOnCSS/MainPage/MainPage.jsx'
+// import Footer from './components/SiteOnCSS/Footer/Footer.jsx'
+// import Header from './components/SiteOnCSS/Header/Header.jsx'
+import PromiseTest from './components/PromiseTest/PromiseTest'
+
+
+function App() {
+
+  return (
+    <div>
+      <PromiseTest/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState, useRef } from 'react';
+import "./Education.css"
+
+const Education = () => {
+    
+
+    return (
+        <div>
+            <a href="#" className='link'>ссылка</a>
+            <input type="text" className='link' placeholder='напиши' disabled/>
+        </div>
+    );
+}
+
+export default Education;
